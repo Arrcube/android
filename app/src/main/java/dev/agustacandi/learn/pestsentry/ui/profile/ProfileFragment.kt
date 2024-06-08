77c195be579e06897b7d23dc13fc2adf6104ce57
Candi Agusta <mugiwaraagusta@gmail.com>
2024-06-08 17:18:41 +0700

    feat: add profile screen

--- app/src/main/java/dev/agustacandi/learn/pestsentry/ui/profile/ProfileFragment.kt
+++ app/src/main/java/dev/agustacandi/learn/pestsentry/ui/profile/ProfileFragment.kt
@@ -1,10 +1,15 @@
 package dev.agustacandi.learn.pestsentry.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import dev.agustacandi.learn.pestsentry.R
 import dev.agustacandi.learn.pestsentry.base.BaseFragment
 import dev.agustacandi.learn.pestsentry.databinding.FragmentProfileBinding
+import dev.agustacandi.learn.pestsentry.utils.ext.showConfirmDialog
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     override fun getViewBinding(
@@ -20,6 +25,35 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     }
 
     override fun initAction() {
+        binding.apply {
+            cardEditProfile.setOnClickListener {
+                findNavController().navigate(R.id.action_profileFragment_to_editProfileFragment)
+            }
+
+            cardPassword.setOnClickListener {
+                findNavController().navigate(R.id.action_profileFragment_to_changePasswordFragment)
+            }
+
+            cardLanguage.setOnClickListener {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+            }
+
+            cardAbout.setOnClickListener {
+                findNavController().navigate(R.id.action_profileFragment_to_aboutFragment)
+            }
+
+            cardLogout.setOnClickListener {
+                showConfirmDialog(
+                    title = getString(R.string.are_you_sure),
+                    message = "",
+                    positiveButtonText = getString(R.string.yes),
+                    negativeButtonText = getString(R.string.no),
+                    onPositiveClick = {
+                        findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+                    }
+                )
+            }
+        }
     }
 
     override fun initProcess() {
